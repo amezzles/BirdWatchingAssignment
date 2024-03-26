202b0d1feca8c57caa71bf4fe950c8a7bb653149
amezzles <141393246+amezzles@users.noreply.github.com>
2024-03-26 14:29:29 +1100

    bird and photo constructors

---
+++ Bird.js
@@ -0,0 +1,12 @@
+
+export default class Bird {
+
+    birdType;
+    position;
+
+    Bird(birdType, position) {
+        this.birdType = birdType;
+        this.position = position;
+    }
+
+}
---
+++ Photo.js
@@ -0,0 +1,12 @@
+import Bird from 'Birds.js';
+
+export default class Photo {
+
+    birdInFrame;
+
+    Photo(birdInFrame) {
+        this.birdInFrame = birdInFrame;
+    }
+
+
+}
