978d29ec605cfb35d7a254a4ab59c4d684836998
amezzles <141393246+amezzles@users.noreply.github.com>
2024-03-26 15:11:40 +1100

    Update BirdCollection.js

--- BirdCollection.js
+++ BirdCollection.js
@@ -1,3 +1,15 @@
+import Bird from './Bird.js';
+
 export default class BirdCollection{
-    
+
+    birds = []; //array of type Bird
+
+    constructor(){
+        return null;
+    }
+
+    updateBirdCollection(bird){
+        this.birds.add 
+    }
+
 }
