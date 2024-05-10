20439b88482b07f7821da414c78112b9c8b350a5
leilamei <justforgamingandmusicstuff@gmail.com>
2024-05-10 14:01:51 +1000

    camera js cleanup

--- Camera.js
+++ Camera.js
@@ -1,5 +1,6 @@
 import Bird from './Bird.js';
 import Photo from './Photo.js';
+import * as THREE from 'three'; // Import THREE if not imported already
 
 export default class Camera {
 
@@ -14,6 +15,12 @@ export default class Camera {
         if (this.birdInFrameBool === true){
             return new Photo();
         }
+    }
+
+    // Function to change the image
+    changeImage() {
+        var img = document.getElementById('image');
+        img.src = "./jsm/images/photoCapture.png";
     }
 
 }
