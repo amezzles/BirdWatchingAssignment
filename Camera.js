a211f93100fe11def45a83e2b2fc143420c24dde
leilamei <justforgamingandmusicstuff@gmail.com>
2024-05-19 17:45:46 +1000

    working on camera bird detection & controls window button

--- Camera.js
+++ Camera.js
@@ -9,18 +9,37 @@ export default class Camera {
 
     constructor() {
         this.birdInFrameBool = false;
+        this.birdInFrame = null; // Initialize birdInFrame to null
+    }
+
+    // Method to set the bird in frame
+    setBirdInFrame(bird) {
+        this.birdInFrame = bird;
+        this.birdInFrameBool = true;
+    }
+
+    // Method to unset the bird in frame
+    unsetBirdInFrame() {
+        this.birdInFrame = null;
+        this.birdInFrameBool = false;
     }
 
     takePhoto() {
-        if (this.birdInFrameBool === true){
-            return new Photo();
+        if (this.birdInFrameBool && this.birdInFrame !== null) {
+            return new Photo(); // Assuming Photo constructor takes no arguments
+        } else {
+            console.log('No bird in frame to capture.');
+            return null;
         }
     }
 
-    // Function to change the image
-    changeImage() {
-        var img = document.getElementById('image');
-        img.src = "./jsm/images/photoCapture.png";
+    // Method to check if the bird is in frame
+    isBirdInFrame() {
+        return this.birdInFrameBool;
     }
 
+    // Method to get the bird in frame
+    getBirdInFrame() {
+        return this.birdInFrame;
+    }
 }
